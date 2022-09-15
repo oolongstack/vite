1ce948eac60bb424b7cc7f4f5d9c88e3e2a6adb2
dduohereshui <2245675739@qq.com>
2022-09-15 23:10:59 +0800

    vite高级用法

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,9 @@ import App from "./App.vue";
 import "./style/index.scss";
 console.log(import.meta.env.VITE_APP_URL);
 
+const globModules = import.meta.glob("./glob/*");
+
+Object.entries(globModules).forEach(([k, v]) => {
+  v().then((module: any) => console.log(k + ":" + module.default));
+});
 createApp(App).mount("#app");
